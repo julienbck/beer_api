ee57c0d3e74e87408129e07e85ce2ecf87706577
Julien Bock <julien.bock@decathlon.com>
2020-02-01 01:57:52 +0100

    refaction method removeduplicated

--- src/Command/LoadDataFixtureCommand.php
+++ src/Command/LoadDataFixtureCommand.php
@@ -4,6 +4,7 @@ namespace App\Command;
 
 use App\Entity\Beer;
 use App\Entity\Brewery;
+use App\Entity\Style;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Id\AssignedGenerator;
 use Doctrine\ORM\Mapping\ClassMetadata;
@@ -25,6 +26,8 @@ class LoadDataFixtureCommand extends Command
     protected $breweries = [];
 
     protected $beers = [];
+
+    protected $styles = [];
 
     protected $em;
 
@@ -63,12 +66,13 @@ class LoadDataFixtureCommand extends Command
 
             $this->parseCsvAndHydrateBreweryEntity($path);
             $totalBreweryLoaded = $this->loadBreweryInDatabase();
+            $totalStyleLoaded = $this->loadStyleInDatabase();
             $this->hydrateBeer();
             $totalBeerLoaded = $this->loadBeerInDatabase();
         }
 
         $totalBrewery = $totalBreweryLoaded ? $totalBreweryLoaded : 0;
-        $this->io->success('We have load '.$totalBrewery.' brewery and load '.$totalBeerLoaded.' beers');
+        $this->io->success('We have load '.$totalBrewery.' brewery and load '.$totalBeerLoaded.' beers and load '.$totalStyleLoaded.' styles');
 
         return 0;
     }
@@ -95,14 +99,14 @@ class LoadDataFixtureCommand extends Command
             }
 
             $this->arrayAssociated[] = $arrayAssociated;
-
-            $this->hydrateBrewery($arrayAssociated);
+            $this->hydrateBreweryAndStyle($arrayAssociated);
         }
 
-        $this->removeDuplicatedBrewery();
+        $this->removeDuplicated('breweries');
+        $this->removeDuplicated('styles');
     }
 
-    private function hydrateBrewery(array $data)
+    private function hydrateBreweryAndStyle(array $data)
     {
         $brewery = new Brewery();
         $brewery
@@ -115,6 +119,18 @@ class LoadDataFixtureCommand extends Command
             ->setUpdatedAt(new \DateTime());
 
         $this->breweries[] = $brewery;
+
+        if ($data['style_id'] > 0) {
+            $style = new Style();
+
+            $style
+                ->setId($data['style_id'])
+                ->setName($data['Style'])
+                ->setCreatedAt(new \DateTime())
+                ->setUpdatedAt(new \DateTime());
+
+            $this->styles[] = $style;
+        }
     }
 
     private function loadBreweryInDatabase(): int
@@ -128,7 +144,7 @@ class LoadDataFixtureCommand extends Command
             $metadata = $this->em->getClassMetaData(get_class($brewery));
             $metadata->setIdGeneratorType(ClassMetadata::GENERATOR_TYPE_NONE);
             $metadata->setIdGenerator(new AssignedGenerator());
-            if($counterFlush % 50  === 0) {
+            if($counterFlush % 100  === 0) {
                 $this->em->flush();
                 $this->em->clear(Brewery::class);
             }
@@ -145,9 +161,38 @@ class LoadDataFixtureCommand extends Command
         return $counterFlush;
     }
 
+    private function loadStyleInDatabase(): int
+    {
+        $this->io->write('Start load style in database', true);
+        $this->io->progressStart(count($this->styles));
+
+        $counterFlush = 0;
+        foreach ($this->styles as $style) {
+            $this->em->persist($style);
+            $metadata = $this->em->getClassMetaData(get_class($style));
+            $metadata->setIdGeneratorType(ClassMetadata::GENERATOR_TYPE_NONE);
+            $metadata->setIdGenerator(new AssignedGenerator());
+            if($counterFlush % 20  === 0) {
+                $this->em->flush();
+                $this->em->clear(Style::class);
+            }
+            $counterFlush++;
+            $this->io->progressAdvance();
+        }
+
+        $this->em->flush();
+        $this->em->clear(Style::class);
+
+        $this->io->progressFinish();
+        $this->io->write('End load style in database', true);
+
+        return $counterFlush;
+    }
+
     private function hydrateBeer()
     {
         $this->breweries = $this->em->getRepository(Brewery::class)->findAll();
+        $this->styles = $this->em->getRepository(Style::class)->findAll();
         foreach ($this->arrayAssociated as $data) {
             $brewery = array_filter(
                 $this->breweries,
@@ -166,10 +211,19 @@ class LoadDataFixtureCommand extends Command
                 ->setCreatedAt(new \DateTime())
                 ->setUpdatedAt(new \DateTime());
 
+            if ($data['style_id'] > 0 ) {
+                $style = array_filter(
+                    $this->styles,
+                    function ($e) use ($data) {
+                        return $e->getId() == $data['style_id'];
+                    }
+                );
+                $beer->setStyle(reset($style));
+            }
+
             $this->beers[] = $beer;
         }
-
-        $this->removeDuplicatedBeer();
+        $this->removeDuplicated('beers');
     }
 
 
@@ -183,7 +237,7 @@ class LoadDataFixtureCommand extends Command
             $metadata = $this->em->getClassMetaData(get_class($beer));
             $metadata->setIdGeneratorType(ClassMetadata::GENERATOR_TYPE_NONE);
             $metadata->setIdGenerator(new AssignedGenerator());
-            if($counterFlush % 50  === 0) {
+            if($counterFlush % 100  === 0) {
                 $this->em->flush();
                 $this->em->clear(Beer::class);
             }
@@ -200,21 +254,12 @@ class LoadDataFixtureCommand extends Command
         return $counterFlush;
     }
 
-    private function removeDuplicatedBeer()
-    {
-        $cleanedBeerArray = [];
-        foreach ($this->beers as $beer) {
-            $cleanedBeerArray[$beer->getId()] = $beer;
-        }
-        $this->beers = $cleanedBeerArray;
-    }
-
-    private function removeDuplicatedBrewery()
+    private function removeDuplicated(string $attribute)
     {
         $cleaned = [];
-        foreach ($this->breweries as $brewery) {
-            $cleaned[$brewery->getId()] = $brewery;
+        foreach ($this->$attribute as $item) {
+            $cleaned[$item->getId()] = $item;
         }
-        $this->breweries = $cleaned;
+        $this->$attribute = $cleaned;
     }
 }
