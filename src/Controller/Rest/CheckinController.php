8592897585317daa1f71fb519d5a109d473ba22b
Julien Bock <julien.bock@decathlon.com>
2020-02-03 15:46:02 +0100

    add collection method checkin

--- src/Controller/Rest/CheckinController.php
+++ src/Controller/Rest/CheckinController.php
@@ -4,9 +4,38 @@
 namespace App\Controller\Rest;
 
 
+use App\Entity\Checkin;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class CheckinController
+class CheckinController extends AbstractRestController
 {
+    public function __construct(SerializerInterface $serializer)
+    {
+        parent::__construct($serializer);
+    }
 
+    /**
+     * @Route("/checkins", name="get_checkins", methods={"GET"})
+     * @param Request $request
+     * @return Response
+     */
+    public function getCollection(Request $request)
+    {
+        $results = $this->getCollectionEntity(Checkin::class, $request->query, ['checkin-collection']);
+
+        if (empty($results['data'])) {
+            return new Response(null, 204);
+        }
+
+
+        $response = new Response($results['data'], 200);
+        $response->headers->set('totalHits', $results['totalHits']);
+        $response->headers->set('totalPage', $results['totalPage']);
+        $response->headers->set('nextPage', $results['nextPage']);
+
+        return $response;
+    }
 }
